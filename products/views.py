c60ae8694925e9c31ca293405bfa44db69906be1
Artem <artemruban762@gmail.com>
2025-01-21 16:41:29 +0200

    Created new views

--- products/views.py
+++ products/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from .models import Product, Category, ProductReview
+from django.core.paginator import Paginator
 
-def index(request):
-    return HttpResponse("hello")
+# Create your views here.
+def home(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    paginator = Paginator(products, 25)
+    page_number = request.GET.get('page')
+    page_products = paginator.get_page(page_number)
+    context = {
+        'products':products,
+        'categories':categories,
+        'page_products':page_products,
+    }
+    return render(request, "home.html", context)
+
+def detail_product(request, slug):
+    product = get_object_or_404(Product, slug = slug)
+    categories = Category.objects.all()
+    context = {
+        'product':product,
+        'categories':categories
+    }
+    return render(request,"detail_product.html", context)
+
+def product_category(request, slug):
+    category = get_list_or_404(Category, slug = slug)
+    products = Product.objects.filter(category = category)
+    categories = Category.objects.all()
+    paginator = Paginator(products, 25)
+    page_number = request.GET.get('page')
+    page_products = paginator.get_page(page_number)
+    context = {
+        'category':category,
+        'categories':categories,
+        'products':products,
+        'page_products':page_products
+    }
+    return render(request,"product_category.html", context)
