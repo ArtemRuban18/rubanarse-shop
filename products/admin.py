20db70b5f3d0a1d74ab26d2e3457dec408c41c30
Artem <artemruban762@gmail.com>
2025-01-21 22:56:31 +0200

    create tables DB in admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product, Category, ProductReview
+from .forms import CreateProduct
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    form = CreateProduct
+    list_display = ['id', 'name', 'description','category','get_type_flavor','volume', 'price', 'quantity', 'available', 'views']
+    search_fields = ['name', 'category', 'get_type_flavor', 'volume']
+    list_filter = ['category', 'price', 'quantity','available']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['name']
+
+@admin.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    model = ProductReview
+    list_display = ['user', 'product', 'comment', 'rating', 'created_at']
+    search_fields = ['product', 'rating']
+    list_filter = ['product', 'rating']
