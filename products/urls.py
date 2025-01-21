b3f6e5126ad6d664dede81b0af47222914026974
Artem <artemruban762@gmail.com>
2025-01-21 16:42:16 +0200

    added new urls

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,7 @@ import products.views as views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/<slug:slug>/', views.detail_product, name='detail_product'),
+    path('products/category/<slug:slug>/', views.product_category, name='product_category'),
+    
 ]
