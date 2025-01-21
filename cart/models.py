e94abc67aeeb99c6406f9bc36e2fa8c95da79e4a
Artem <artemruban762@gmail.com>
2025-01-21 22:57:23 +0200

    create Cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"cart for {self.user} was created"
