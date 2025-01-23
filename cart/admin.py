03c85004a7b07a75a90c8fa0c325a2bbf3eb9366
Artem <artemruban762@gmail.com>
2025-01-23 22:01:44 +0200

    models Cart and CartProduct was added to admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Cart, CartProduct
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at']
+
+@admin.register(CartProduct)
+class CartProduct(admin.ModelAdmin):
+    list_display = ['cart', 'product', 'quantity', 'total_price']
+
+    def total_price(self, obj):
+        return obj.product.price * obj.quantity
+    
+    total_price.short_description = "Загальна вартість"
