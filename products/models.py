1ee9a11138b9da2d11c7af05e1734e778ef5645c
Artem <artemruban762@gmail.com>
2025-01-23 22:00:40 +0200

    changed model Product and added model TypeFlavor

--- products/models.py
+++ products/models.py
@@ -13,42 +13,37 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class TypeFlavor(models.Model):
+    name = models.CharField(max_length=255, blank=False, verbose_name="Аромат")
+    slug = models.SlugField(max_length=255, blank=True)
+
+    def save(self, *args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
     
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
-    TYPE_OF_FLAVOR = [
-    ('floral', 'Квітковий'),
-    ('fruity', 'Фруктовий'),
-    ('woody', 'Деревний'),
-    ('chypre', 'Шипровий'),
-    ('oriental', 'Східний'),
-    ('citrus', 'Цитрусовий'),
-    ('spicy', 'Пряний'),
-    ('marine', 'Морський'),
-    ('vanilla', 'Ванільний')
-]
-
-
     name = models.CharField(max_length= 255, blank = False, verbose_name='Назва')
     description = models.TextField(blank = False,verbose_name='Опис')
     price = models.DecimalField(max_digits = 10, decimal_places = 2, blank=False, verbose_name='Ціна')
     category = models.ForeignKey(Category, on_delete = models.CASCADE, blank = False, related_name='products', verbose_name='Категорія')
-    type_flavor = models.TextField(blank = False, verbose_name='Аромат', default='-')
+    type_flavor = models.ManyToManyField(TypeFlavor, related_name="flavors",  verbose_name='Аромат')
     quantity = models.PositiveIntegerField(blank = False, verbose_name='Кількість')
     image = models.ImageField(upload_to = 'images/', blank = False)
     volume = models.CharField(max_length = 100, blank = False, verbose_name="Об'єм(мл)")
     slug = models.SlugField(max_length = 255, blank = False, unique= True)
-    available = models.BooleanField(default=True, blank=False)
-    views = models.PositiveIntegerField(default=0)
+    available = models.BooleanField(default=True, blank=False,  verbose_name='Доступно')
+    views = models.PositiveIntegerField(default=0,  verbose_name='Кількість переглядів')
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
         self.available = self.quantity > 0
         super().save(*args, **kwargs)
-
-    def get_type_flavor(self):
-        flavors = self.type_flavor.split(",") 
-        return ", ".join(dict(self.TYPE_OF_FLAVOR).get(flavor.strip(), flavor) for flavor in flavors)
     
     def __str__(self):
         return self.name
