8d9167a94a49e5cf3e0e37c4fcabe1f885b4a3e1
Artem <artemruban762@gmail.com>
2025-01-23 21:59:28 +0200

    created models cart

--- cart/models.py
+++ cart/models.py
@@ -3,9 +3,22 @@ from products.models import Product
 from django.contrib.auth.models import User
 
 class Cart(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    products = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
-    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Користувач')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата створення')
+
+    def total_price(self):
+        return sum(product.total_price() for product in self.product.all())
 
     def __str__(self):
-        return f"cart for {self.user} was created"
+        return f"Кошик {self.user}"
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='products',  verbose_name='Кошик')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,  verbose_name='Назва товару')
+    quantity = models.PositiveIntegerField(blank=False,  verbose_name='Кількість')
+
+    def total_price(self):
+        return self.product * self.quantity
+    
+    def __str__(self):
+        return f"Додано {self.product} - {self.quantity}"
