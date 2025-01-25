a62c4a145e22b6c337ba91cf82c50d3e0d3e7ff9
Artem <artemruban762@gmail.com>
2025-01-25 22:28:00 +0200

    Created views 'password_change' and 'password_reset'

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import SignUp
-from django.contrib.auth.views import LoginView
+from .forms import SignUp, CustomPasswordChangeForm, CustomPasswordResetForm
+from django.contrib.auth.decorators import login_required
 
 def signup(request):
     if request.method == 'POST':
@@ -10,4 +10,26 @@ def signup(request):
             return redirect(request, 'login', {'signup_form':signup_form})
         else:
             signup_form = SignUp()
-    return render(request, 'signup.html', {'signup_form':signup_form}) 
+    return render(request, 'signup.html', {'signup_form':signup_form})
+
+@login_required
+def password_change(request):
+    if request.method == 'POST':
+        password_change_form = CustomPasswordChangeForm(request.POST, user = request.user)
+        if password_change_form.is_valid():
+            password_change_form.save()
+            return redirect('password_change_done')
+    else:
+        password_change_form = CustomPasswordChangeForm(user = request.user)
+    return render(request, 'password_change.html', {'password_change_form':password_change_form})
+
+
+def password_reset(request):
+    if request.method == 'POST':
+        password_reset_form = CustomPasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            password_reset_form.save()
+            return redirect('password_reset_done')
+    else:
+        password_reset_form = CustomPasswordResetForm()
+    return render(request, 'password_reset.html', {'password_reset_form':password_reset_form})
