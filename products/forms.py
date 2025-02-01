0b19256b24439e338eef1aa73fe1db751ce0c543
Artem <artemruban762@gmail.com>
2025-02-02 01:24:12 +0200

    create form for review the product

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,11 @@
 from django import forms
+from .models import ProductReview
 
 class SearchForm(forms.Form):
     query = forms.CharField()
+
+class ProductReviewForm(forms.ModelForm):
+    class Meta:
+        model = ProductReview
+        fields = ['rating', 'comment']
+        widget = {'rating': forms.HiddenInput(),}
