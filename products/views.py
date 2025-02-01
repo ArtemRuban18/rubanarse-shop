565cb1e43fbfe61d68d27e97990fb32f0b0cc971
Artem <artemruban762@gmail.com>
2025-02-02 01:29:07 +0200

    added ability to leave comments for product and created view for search products

--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
-from .models import Product, Category, ProductReview, TypeFlavor
+from .models import Product, Category, TypeFlavor, ProductReview
 from django.core.paginator import Paginator
 from django.core.cache import cache
-from .forms import SearchForm
+from .forms import SearchForm, ProductReviewForm
 from .filters import ProductFilter
 
 def home(request):
@@ -29,9 +29,24 @@ def detail_product(request, slug):
     product.views += 1
     product.save()
     categories = Category.objects.all()
+    try:
+        review = ProductReview.objects.get(user = request.user, product = product)
+        review_form = ProductReviewForm(request.POST or None, instance=review)
+    except ProductReview.DoesNotExist:
+        review_form = ProductReviewForm(request.POST or None)
+    if request.method == 'POST':
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.product = product
+            review.save()
+            return redirect('detail_product', slug = product.slug)
+    else:
+        review_form = ProductReviewForm()
     context = {
         'product':product,
-        'categories':categories
+        'categories':categories,
+        'review_form':review_form,
     }
     return render(request,"detail_product.html", context)
 
@@ -53,3 +68,14 @@ def product_category(request, slug):
     }
     return render(request,"product_category.html", context)
 
+def search(request):
+    search_form = SearchForm(request.GET)
+    result = []
+    if search_form.is_valid():
+        query = search_form.cleaned_data['query']
+        result = Product.objects.filter(name__icontains = query) | Product.objects.filter(description__icontains = query)
+    context = {
+        'search_form':search_form,
+        'result':result
+    }
+    return render(request, 'search.html', context)
