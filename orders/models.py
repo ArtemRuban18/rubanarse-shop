40f09a04c08ffc7d09b603d4bb71736a2f4474db
Artem <artemruban762@gmail.com>
2025-02-06 00:52:40 +0200

    added field status in Order model and created method confirm order

--- orders/models.py
+++ orders/models.py
@@ -8,16 +8,34 @@ class Order(models.Model):
         ('Full subscription', 'Повна передоплата'),
     ]
 
+    STATUS_ORDER = [
+        ('pending','в обробці'),
+        ('confirmed','підтверджено'),
+        ('cancelled','скасовано'),
+    ]
+
     order_id = models.PositiveIntegerField(blank=False, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=255, blank=False)
     phone = models.PositiveIntegerField(blank=False)
     email = models.EmailField(blank=False)
     payment_method = models.CharField(choices=PAYMENT_METHOD, default='upon receipt')
+    status = models.CharField(choices=STATUS_ORDER, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def total_price(self):
         return sum(product.total_price() for product in self.product.all())
+    
+    def confirm_order(self):
+        if self.status == 'pending':
+            for product_order in self.products.all():
+                product = product_order.product
+                if product.quantity < product_order.quantity:
+                    raise ValueError(f"Недостатня кількість товару на складі {product.name}")
+                product.quantity -= product_order.quantity
+                product.save()
+            self.status = 'confirmed'
+            self.save()
     
     def __str__(self):
         return f"{self.order_id}"
