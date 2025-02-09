63c2fff7ced5c648dc8e4a4c9ac54d7322647364
Artem <artemruban762@gmail.com>
2025-02-10 01:05:01 +0200

    completed problems

--- orders/models.py
+++ orders/models.py
@@ -20,10 +20,7 @@ class Order(models.Model):
     phone = models.PositiveIntegerField(blank=False)
     email = models.EmailField(blank=False)
     payment_method = models.CharField(choices=PAYMENT_METHOD, default='upon receipt')
-<<<<<<< HEAD
     comment = models.CharField(blank=False, default='')
-=======
->>>>>>> 2f86da1a4fb9b380c19b191a6c4c4cd4dd535fcc
     status = models.CharField(choices=STATUS_ORDER, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -35,16 +32,14 @@ class Order(models.Model):
             for product_order in self.products.all():
                 product = product_order.product
                 if product.quantity < product_order.quantity:
-<<<<<<< HEAD
                     raise ValueError(f"Недостатня кількість товару {product.name} на складі.")
-=======
+
                     raise ValueError(f"Недостатня кількість товару на складі {product.name}")
->>>>>>> 2f86da1a4fb9b380c19b191a6c4c4cd4dd535fcc
                 product.quantity -= product_order.quantity
                 product.save()
             self.status = 'confirmed'
             self.save()
-<<<<<<< HEAD
+
 
     def cancel_order(self):
         if self.status != 'pending':
@@ -55,10 +50,6 @@ class Order(models.Model):
             product.save()
         self.status == 'cancelled'
         self.save()
-            
-=======
-    
->>>>>>> 2f86da1a4fb9b380c19b191a6c4c4cd4dd535fcc
     def __str__(self):
         return f"{self.order_id}"
     
