1ed206063e6b8c6b63fe7abeda56d1866b097ad8
Artem <artemruban762@gmail.com>
2025-02-10 00:53:57 +0200

    added comment field in CreateOrderForm

--- orders/forms.py
+++ orders/forms.py
@@ -5,11 +5,12 @@ from django.forms import inlineformset_factory
 class CreateOrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['full_name', 'phone', 'email']
+        fields = ['full_name', 'phone', 'email', 'comment']
         labels = {
             'full_name':'Прізвище Ім\'я',
             'phone':'Номер телефону',
             'email':'Електрона пошта',
+            'comment':'Введіть адресу доставки'
         }
 
 class EditOrderProductForm(forms.ModelForm):
