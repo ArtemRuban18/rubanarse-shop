86fee4bba3c9634252ff13a313177ce424ead7e4
Artem <artemruban762@gmail.com>
2025-02-17 23:10:02 +0200

    upgrade urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import add_to_cart, detail_cart, delete_from_cart
 urlpatterns = [
     path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    path('detail/cart/<str:username>/', detail_cart, name='detail_cart'),
+    path('cart/<str:username>/', detail_cart, name='detail_cart'),
     path('delete-product-from-cart/<int:product_id>/', delete_from_cart, name='delete_product'),
 ]
