b5eab5e8cb5b7a7612b6b797c93b154b82d4e884
Artem <artemruban762@gmail.com>
2025-02-17 23:17:31 +0200

    create static files and templates and upgrade urls.py and views.py

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ import products.views as views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/<slug:slug>/', views.detail_product, name='detail_product'),
-    path('products/category/<slug:slug>/', views.product_category, name='product_category'),
+    path('products/category/<slug:slug>/', views.product_by_category, name='product_by_category'),
+    path('products/flavor/<slug:slug>/', views.product_by_flavor, name='product_by_flavor'),
     path('search/', views.search, name = 'search'),
 ]
--- products/views.py
+++ products/views.py
@@ -4,31 +4,32 @@ from django.core.paginator import Paginator
 from django.core.cache import cache
 from .forms import SearchForm, ProductReviewForm
 from .filters import ProductFilter
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 def home(request):
-    products = cache.get('product_list')
+    cache_key = 'product_list'
+    products = cache.get(cache_key)
     if not products:
         products = Product.objects.all()
-        cache.set('product_list', products, timeout = 60*10)
+        cache.set(cache_key,products, timeout = 60*10)
     filter = ProductFilter(request.GET, queryset=products)
-    categories = Category.objects.all()
-    paginator = Paginator(products, 25)
+    paginator = Paginator(filter.qs, 1)
     page_number = request.GET.get('page')
     page_products = paginator.get_page(page_number)
     context = {
         'products':products,
-        'categories':categories,
         'page_products':page_products,
         'filter':filter,
 
     }
     return render(request, "home.html", context)
 
+@login_required
 def detail_product(request, slug):
     product = get_object_or_404(Product, slug = slug)
     product.views += 1
     product.save()
-    categories = Category.objects.all()
     try:
         review = ProductReview.objects.get(user = request.user, product = product)
         review_form = ProductReviewForm(request.POST or None, instance=review)
@@ -45,37 +46,70 @@ def detail_product(request, slug):
         review_form = ProductReviewForm()
     context = {
         'product':product,
-        'categories':categories,
         'review_form':review_form,
     }
     return render(request,"detail_product.html", context)
 
-def product_category(request, slug):
-    category = get_list_or_404(Category, slug = slug)
-    products = cache.get('product_category')
+def product_by_category(request, slug):
+    category = get_object_or_404(Category, slug = slug)
+    cache_key = f'product_category_{slug}'
+    products = cache.get(cache_key)
     if not products:
         products = Product.objects.filter(category = category)
-        cache.set(products, 'product_category', timeout=60*20)
-    categories = Category.objects.all()
-    paginator = Paginator(products, 25)
+        cache.set(cache_key,products, timeout=60*20)
+    filter = ProductFilter(request.GET,queryset=products)
+    paginator = Paginator(filter.qs, 1)
     page_number = request.GET.get('page')
     page_products = paginator.get_page(page_number)
     context = {
         'category':category,
-        'categories':categories,
         'products':products,
         'page_products':page_products,
+        'filter':filter
     }
     return render(request,"product_category.html", context)
 
+def product_by_flavor(request, slug):
+    flavor = get_object_or_404(TypeFlavor, slug = slug)
+    cache_key = f'product_category_{slug}'
+    products = cache.get(cache_key)
+    if not products:
+        products = Product.objects.filter(type_flavor = flavor)
+        cache.set(cache_key,products, timeout=60*20)
+    filter = ProductFilter(request.GET, queryset=products)
+    paginator = Paginator(filter.qs, 25)
+    page_number = request.GET.get('page')
+    page_products = paginator.get_page(page_number)
+    context = {
+        'flavor':flavor,
+        'products':products,
+        'page_products':page_products,
+        'filter':filter
+    }
+    return render(request,"product_flavor.html", context)
+
 def search(request):
     search_form = SearchForm(request.GET)
-    result = []
+    result = Product.objects.none()
+
     if search_form.is_valid():
         query = search_form.cleaned_data['query']
-        result = Product.objects.filter(name__icontains = query) | Product.objects.filter(description__icontains = query)
+        search_result = Q(
+            name__icontains = query
+        ) | Q(description__icontains = query
+        ) | Q(category__name__icontains = query
+        ) | Q(type_flavor__name__icontains = query)
+        result = Product.objects.filter(search_result).distinct()
+    
+    filter = ProductFilter(request.GET, queryset=result)
+    paginator = Paginator(filter.qs, 25)
+    page_number = request.GET.get('page')
+    page_products = paginator.get_page(page_number)
+        
     context = {
         'search_form':search_form,
-        'result':result
+        'result':result,
+        'page_products':page_products,
+        'filter':filter
     }
     return render(request, 'search.html', context)
