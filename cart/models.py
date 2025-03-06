5b2ca6626fbfa33224025b1e688a0f43aabc1b88
Artem <artemruban762@gmail.com>
2025-03-06 23:14:56 +0200

    add doctrings

--- cart/models.py
+++ cart/models.py
@@ -3,16 +3,33 @@ from products.models import Product
 from django.contrib.auth.models import User
 
 class Cart(models.Model):
+    """
+    Model for cart of users
+
+    Fields:
+        -user: Foreign key to User Model
+        - created_at: creation date(DateTimeField)
+    """
+    
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Користувач')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата створення')
 
     def total_price(self):
-        return sum(product.total_price() for product in self.product.all())
+        return sum(product.total_price() for product in self.products.all())
 
     def __str__(self):
         return f"Кошик {self.user}"
 
 class CartProduct(models.Model):
+    """
+    Model for products in cart
+    
+    Fields:
+        - cart: cart o user(Foreign Key)
+        - product: product in cart(Foreign Key)
+        - quantity: quantity of product in cart(PositiveIntegerField)
+    """
+
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='products',  verbose_name='Кошик')
     product = models.ForeignKey(Product, on_delete=models.CASCADE,  verbose_name='Назва товару')
     quantity = models.PositiveIntegerField(blank=True,  verbose_name='Кількість', default = 0)
@@ -22,17 +39,3 @@ class CartProduct(models.Model):
     
     def __str__(self):
         return f"Додано {self.product} - {self.quantity}"
-
-class Like(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Користувач')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата створення')
-
-    def __str__(self):
-        return f"Вподобано {self.user}"
-
-class LikeProduct(models.Model):
-    like = models.ForeignKey(Like, on_delete=models.CASCADE, related_name='products',  verbose_name='Вподобане')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE,  verbose_name='Назва товару')
-    
-    def __str__(self):
-        return f"Додано {self.product}"
