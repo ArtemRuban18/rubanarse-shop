86fa73d860f597a307291059d767fa353ce641a9
Artem <artemruban762@gmail.com>
2025-03-06 23:28:39 +0200

    add docstrings

--- products/filters.py
+++ products/filters.py
@@ -2,13 +2,22 @@ import django_filters
 from .models import Product, Category, TypeFlavor
 
 class ProductFilter(django_filters.FilterSet):
+    """
+    Filter for products
+
+    Fields:
+        - name: CharField
+        - price__lte: NumberField
+        - price__gte: NumberField
+        - category: ModelChoiceField
+        - type_flavor: ModelChoiceField
+    """
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
-    price = django_filters.NumberFilter()
-    price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
-    price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
+    price__lte = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
+    price__gte = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
     category = django_filters.ModelChoiceFilter(queryset = Category.objects.all(), field_name = 'category', label = 'Категорія')
     type_flavor = django_filters.ModelChoiceFilter(queryset = TypeFlavor.objects.all(), field_name = 'type_flavor', label = 'Аромат')
 
     class Meta:
         model = Product
-        fields = ['name', 'price', 'category', 'type_flavor']
+        fields = ['name', 'price__gte', 'price__lte', 'category', 'type_flavor']
