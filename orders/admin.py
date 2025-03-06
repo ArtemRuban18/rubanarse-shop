405355bc661e0dc7656bf7115a3b06313db0671b
Artem <artemruban762@gmail.com>
2025-03-06 23:20:59 +0200

    add docstrings

--- orders/admin.py
+++ orders/admin.py
@@ -3,15 +3,24 @@ from .models import Order, OrderProduct
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['order_id','full_name','phone', 'email','status', 'created_at']
+    """
+    Admin model for Order model
+    """
+
+    list_display = ['order_id', 'full_name', 'phone', 'email', 'status', 'payment_method','total_price', 'created_at']
     ordering = ['created_at']
     list_filter = ['order_id', 'email', 'full_name','status']
     search_fields = ['full_name', 'email', 'order_id']
+    readonly_fields = ('total_price',)
 
 @admin.register(OrderProduct)
 class OrderProductAdmin(admin.ModelAdmin):
+    """
+    Admin class for OrderProduct model
+    """
     list_display = ['order', 'product', 'quantity', 'total_price']
     search_fields = ['order__order_id']
+    readonly_fields = ('total_price',)
 
     def total_price(self, obj):
         return obj.product.price * obj.quantity
