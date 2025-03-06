fb91e4c8ac1b1f62a67ca9c669bb87ba625fe6f5
Artem <artemruban762@gmail.com>
2025-03-06 23:14:21 +0200

    add docstrings

--- cart/admin.py
+++ cart/admin.py
@@ -1,23 +1,25 @@
 from django.contrib import admin
-from .models import Cart, CartProduct, Like, LikeProduct
+from .models import Cart, CartProduct
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
+    """
+    Admin class for Cart model
+    """
     list_display = ['user', 'created_at']
 
 @admin.register(CartProduct)
 class CartProductAdmin(admin.ModelAdmin):
+    """
+    Admin class for CartProduct model
+
+    Methods:
+        - total_price: Returns total price of product
+    """
+    
     list_display = ['cart', 'product', 'quantity', 'total_price']
 
     def total_price(self, obj):
         return obj.product.price * obj.quantity
     
     total_price.short_description = "Загальна Сума"
-
-@admin.register(Like)
-class LikeAdmin(admin.ModelAdmin):
-    list_display = ['user', 'created_at']
-
-@admin.register(LikeProduct)
-class LikeProductAdmin(admin.ModelAdmin):
-    list_display = ['like', 'product']
