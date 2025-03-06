72faab97120f3c7cf7790dc0e7a9e8c2cee3fa7d
Artem <artemruban762@gmail.com>
2025-03-06 23:29:53 +0200

    add docstrings

--- products/models.py
+++ products/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class Category(models.Model):
+    """
+    Model for category of products
+
+    Fieds:
+        - name: name of category(CharField)
+        - slug: slug of category(SlugField)    
+    """
+
     name = models.CharField(max_length = 100, unique= True, blank = False, verbose_name='Назва')
     slug = models.SlugField(max_length = 100, unique = True, blank = False)
 
@@ -15,6 +24,14 @@ class Category(models.Model):
         return self.name
 
 class TypeFlavor(models.Model):
+    """
+    Model for type of flavor for products
+    
+    Fields:
+        - name: name of type flavors(CharFiled)
+        - slug: slug of type flavors(SlufField)
+    """
+
     name = models.CharField(max_length=255, blank=False, verbose_name="Аромат")
     slug = models.SlugField(max_length=255, blank=True)
 
@@ -27,6 +44,23 @@ class TypeFlavor(models.Model):
         return self.name
 
 class Product(models.Model):
+    """
+    Model for products
+
+    Fields:
+        - name: name of product(CharField)
+        - description: description of product(TextField)
+        - price: price of product(DecimalField)
+        - category: category of product(ForeignKey)
+        - type_flavor: type of flavor for product(ManyToManyField)
+        - quantity: quantity of product(PositiveIntegerField)
+        - image: image of product(ImageField)
+        - volume: volume of product(CharField)
+        - slug: slug of product(SlugField)
+        - available: availability of product(BooleanField)
+        - views: views of product(PositiveIntegerField)
+    """
+
     name = models.CharField(max_length= 255, blank = False, verbose_name='Назва')
     description = models.TextField(blank = False,verbose_name='Опис')
     price = models.DecimalField(max_digits = 10, decimal_places = 2, blank=False, verbose_name='Ціна')
@@ -38,7 +72,6 @@ class Product(models.Model):
     slug = models.SlugField(max_length = 255, blank = False, unique= True)
     available = models.BooleanField(default=True, blank=False,  verbose_name='Доступно')
     views = models.PositiveIntegerField(default=0,  verbose_name='Кількість переглядів')
-    likes = models.PositiveIntegerField(default=0, verbose_name='Кількість вподобань')
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -46,11 +79,28 @@ class Product(models.Model):
         self.available = self.quantity > 0
         super().save(*args, **kwargs)
     
+    def clean(self):
+        if self.quantity < 0:
+            raise ValidationError({'quantity': 'Quantity cannot be negative.'})
+        if self.price < 0:
+            raise ValidationError({'price': 'Price cannot be negative'})
+    
     def __str__(self):
         return self.name
 
 
 class ProductReview(models.Model):
+    """
+    Model for reviews of products
+    
+    Fields:
+        - user: user who left a review(ForeignKey)
+        - product: product for which the review was left(ForeignKey)
+        - rating: rating of product(PositiveIntegerField)
+        - comment: comment of user(TextField)
+        - created_at: date of creation of review(DateTimeField)
+    """
+
     CHOICES_RATING = (
         (1, '1'),
         (2, '2'),
@@ -64,6 +114,10 @@ class ProductReview(models.Model):
     rating = models.PositiveIntegerField(choices = CHOICES_RATING, verbose_name='Оцінка', blank=False)
     comment = models.TextField(blank = True, verbose_name='Коментар')
     created_at = models.DateTimeField(auto_now_add = True, verbose_name='Створено')
+
+    def clean(self):
+        if self.rating < 1 or self.rating > 5:
+            raise ValidationError({'rating': 'Rating must be between 1 and 5.'})
 
     def __str__(self):
         return self.product.name
