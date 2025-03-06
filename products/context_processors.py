0843bc152633e11cd0d0bb2defc406c91dac02ca
Artem <artemruban762@gmail.com>
2025-03-06 23:27:59 +0200

    add docstrings

--- products/context_processors.py
+++ products/context_processors.py
@@ -1,6 +1,9 @@
 from .models import Category, TypeFlavor
 
 def global_context(request):
+    """
+    Context processor for flobal variables
+    """
     return {
         'categories': Category.objects.all(),
         'flavors': TypeFlavor.objects.all()
