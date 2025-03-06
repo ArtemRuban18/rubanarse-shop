f23bee393841e9fff382e5051e40c2da4ba76616
Artem <artemruban762@gmail.com>
2025-03-06 23:16:34 +0200

    add delete_product url

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import add_to_cart, detail_cart, delete_from_cart
+from .views import *
 urlpatterns = [
     path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('cart/<str:username>/', detail_cart, name='detail_cart'),
