d8e488562e2aa9a0855df1acfe935e3d8957b10e
Artem <artemruban762@gmail.com>
2025-03-06 23:08:02 +0200

    add docstrings for forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,6 +5,15 @@ from django.core.exceptions import ValidationError
 from django.conf import settings
 
 class SignUp(UserCreationForm):
+    """
+    Form for user registration
+
+    Fields:
+        - username: CharField
+        - email: EmailField
+        - password1: CharField
+        - password2: CharField
+    """
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
@@ -21,6 +30,12 @@ class SignUp(UserCreationForm):
             'password2':None,
         }
 class CustomPasswordResetForm(PasswordResetForm):
+    """
+    Form for password reset
+
+    Fields:
+        - email: EmailField
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['email'].widget = forms.EmailInput(attrs={"autocomplete": "email"})
