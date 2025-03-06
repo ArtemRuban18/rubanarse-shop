4937ab81de04e8cc59c53fbeda8b13dd5c2841b9
Artem <artemruban762@gmail.com>
2025-03-06 23:24:26 +0200

    add information_about_order url

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import create_order, detail_order, confirm_order, cancel_order
+from .views import *
 
 urlpatterns = [
     path('create-order/', create_order, name='create_order'),
     path('order/<int:order_id>/', detail_order, name='detail_order'),
     path('confirm-order/<int:order_id>/', confirm_order, name='confirm_order'),
-    path('order/<int:order_id>/cancel/', cancel_order, name='cancel_order'),
+    path('information-about-order/<int:order_id>/', information_about_order, name='information_about_order'),
 ]
