88f29db9a45be1e59926a58bd364cbb098265ca4
Artem <artemruban762@gmail.com>
2025-03-06 23:21:35 +0200

    add docstrings

--- orders/forms.py
+++ orders/forms.py
@@ -1,19 +1,35 @@
 from django import  forms
 from .models import Order, OrderProduct
-from django.forms import inlineformset_factory
 
 class CreateOrderForm(forms.ModelForm):
+    """
+    Form for creating order
+
+    Fields:
+        - full_name: CharField
+        - phone: CharField
+        - email: EmailField
+        - comment: CharField
+        - payment_method: CharField
+    """
     class Meta:
         model = Order
-        fields = ['full_name', 'phone', 'email', 'comment']
+        fields = ['full_name', 'phone', 'email', 'comment','payment_method']
         labels = {
             'full_name':'Прізвище Ім\'я',
             'phone':'Номер телефону',
             'email':'Електрона пошта',
-            'comment':'Введіть адресу доставки'
+            'comment':'Введіть адресу доставки(Місто, область, відділення пошти)',
+            'payment_method':'Спосіб оплати'
         }
 
 class EditOrderProductForm(forms.ModelForm):
+    """
+    Form for editing order product
+    
+    Fields:
+        - quantity
+    """
     class Meta:
         model = OrderProduct
         fields = ['quantity']
